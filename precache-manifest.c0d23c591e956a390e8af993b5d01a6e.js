e5b37ee4395af49dfbcc21a18ea846e90c2164eb
dmistry <dipen.mistry@talentica.com>
2020-04-18 17:14:50 +0530

    Updates

---
+++ precache-manifest.c0d23c591e956a390e8af993b5d01a6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ab017df6e8b78ab0b0397b584520fd",
+    "url": "/dipen-app/index.html"
+  },
+  {
+    "revision": "7e520c682a552041b413",
+    "url": "/dipen-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "19cb2b0f08294ef21790",
+    "url": "/dipen-app/static/js/2.798be310.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dipen-app/static/js/2.798be310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e520c682a552041b413",
+    "url": "/dipen-app/static/js/main.0a5b11e7.chunk.js"
+  },
+  {
+    "revision": "65e5d217897f4dc233f2",
+    "url": "/dipen-app/static/js/runtime-main.7c11c68d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dipen-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
